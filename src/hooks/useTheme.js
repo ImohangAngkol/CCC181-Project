369e150dbdc2bdb3f925e7b24cbb4f8c8cfa10cb
Andrew Daniel C. Corpuz <andrewdaniel.corpuz@g.msuiit.edu.ph>
2025-12-05 23:19:05 +0800

    Delete src directory

--- src/hooks/useTheme.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { ThemeContext } from "../context/ThemeContext.jsx";
-
-/**
- * useTheme - consume theme context
- */
-export default function useTheme() {
-  return useContext(ThemeContext);
-}
